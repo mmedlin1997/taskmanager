56c2ac4c8004e77af09fb1c9de94f0821c992ef8
Mark Medlin <mmedlin1997@yahoo.com>
2020-09-15 00:27:01 -0700

    updated config for Docker container

--- config.py
+++ config.py
@@ -11,11 +11,15 @@ class Config:
     FLASK_ENV = 'development'
     TESTING = True
     DEBUG = True
-    SERVER_NAME = 'localhost:5000'
+    SERVER_NAME = '0.0.0.0:5000'
 
     # Set DB Variables
     MYSQL_USER = environ.get('MYSQL_USER')
     MYSQL_PASSWORD = environ.get('MYSQL_PASSWORD')
     MYSQL_CURSORCLASS = 'DictCursor'
     MYSQL_DB = 'taskdb'
+    MYSQL_HOST = 'task-mysql'
+    MYSQL_PORT = 3306
+    #MYSQL_ROOT_HOST = 127.0.0.1
+    #MYSQL_ROOT_PASSWORD = 'NAt1ss4m1gl4m'
     
